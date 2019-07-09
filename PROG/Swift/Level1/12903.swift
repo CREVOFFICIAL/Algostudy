d8b89c0d2cfbec16cdf798d3c667059a28855540
junyng <pom0894@naver.com>
2019-07-09 16:35:13 +0900

    가운데 글자 가져오기 문제 해결

---
+++ PROG/Swift/Level1/12903.swift
@@ -0,0 +1,7 @@
+func solution(_ s:String) -> String {    
+    if s.count % 2 == 0 {
+        return "\(s[s.index(s.startIndex, offsetBy: s.count / 2 - 1)])\(s[s.index(s.startIndex, offsetBy: s.count / 2)])"
+    } else {
+        return "\(s[s.index(s.startIndex, offsetBy: s.count / 2)])"
+    }
+}
